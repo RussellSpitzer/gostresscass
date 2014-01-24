e6b73616bfb8d58e9e43eb41bfbdfa219bb6ad2e
Russell Spitzer <Russell.Spitzer@gmail.com>
2014-01-23 21:03:04 -0800

    Scan can't be used like a prepared statement

--- main.go
+++ main.go
@@ -62,11 +62,11 @@ func stress(num_ops int) {
 }
 
 func insert(session *gocql.Session) {
-	query := session.Query(`INSERT INTO stress_table (key1,key2,key3,col1,col2,col3) VALUES (?,?,?,?,?,?)`)
 	for value := range ops_insert {
-		if err := query.Scan(&value[0], &value[1], &value[2], &value[3], &value[4], &value[5]); err != nil {
+		query := session.Query(`INSERT INTO stress_table (key1,key2,key3,col1,col2,col3) VALUES (?,?,?,?,?,?)`, value[0], value[1], value[2], value[3], value[4], value[5])
+		if err := query.Exec(); err != nil {
+			log.Fatal(err)
 		}
-
 	}
 	done <- true
 
